c2a981bfdfe0b707cf53f534621762de1f353440
swaldhelm@gmx.de <swaldhelm@gmx.de>
2017-07-19 08:46:29 +0200

    Initialize Test

---
+++ GoFbPixel.php
@@ -0,0 +1,26 @@
+<?php
+namespace GoFbPixel;
+
+use Shopware\Components\Plugin;
+//use Shopware\Components\Plugin\Context\InstallContext;
+//use Shopware\Components\Plugin\Context\UninstallContext;
+
+class GoFbPixel extends \Shopware\Components\Plugin
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+                'Enlight_Controller_Action_PreDispatch_Frontend' => 'preparePlugin',
+        ];
+    }
+    
+    public function preparePlugin()
+    {
+        $this->container->get('Template')->addTemplateDir(
+            $this->getPath() . '/Resources/views/'
+        );
+        echo "Test";
+    }
+
+}
+?>
